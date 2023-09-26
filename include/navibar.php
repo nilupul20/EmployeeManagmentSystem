0113e4f240d0221dc3cb9e241be90f0f8bc3b074
nilupul20 <nilupul.20@cse.mrt.ac.lk>
2023-09-26 12:52:27 +0530

    initial commit: completed project

---
+++ include/navibar.php
@@ -0,0 +1,7 @@
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Hello <?php echo $_SESSION['user_name']; ?> !</span>
+    <a href="../../index.php" class="btn btn-danger">Logout</a>
+  </div>
+</nav>
+
